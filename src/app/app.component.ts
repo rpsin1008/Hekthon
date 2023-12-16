c9ac08aadbca65d9acfc5654ffb8448a9490a14b
Dinesh Singh <dinesh.singh@ecolab.com>
2023-12-16 20:05:36 +0530

    Added code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private fb: FormBuilder){
+    this.typeOfSchudule = ['No Recurrence', 'Hourly', 'Daily'];
+    
+  }
+  
   title = 'createChecklistFormMobile';
+  time = { hour: 13, minute: 30 };
+  standalone = true;
+	meridian = true;
+  typeOfSchudule: Array<string> | undefined;
+  model   = {option: 'option1'};
+
+  
+  brand = [
+    {id: 1, name: 'ABC'},
+    {id: 2, name: 'XYZ'}
+];
+  checklistType = [
+    {id: 1, name: 'cooling'},
+    {id: 2, name: 'cooking'}
+];
+role = [
+  {id: 1, name: 'Associate'},
+  {id: 2, name: 'Manager'}
+];
+areas = [
+  {id: 1, name: 'Pest'},
+  {id: 2, name: 'Petrol'}
+];
+
+ngOnInit(): void {
+  this.initializeForm();
+ 
+}
+
+
+
+initializeForm() {
+  this.changeSchuduleType(this.model);
+}
+
+  
+changeSchuduleType(getSchuduleTye: any) {
+    
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbAlertModule, NgbDatepickerModule, NgbModule, NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbTimepickerModule,
+    FormsModule,
+    NgSelectModule,
+    NgbDatepickerModule, 
+    NgbAlertModule, 
+    JsonPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
